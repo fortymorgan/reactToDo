922fa38d7b178013f7381053fc8f52a886653e2e
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-21 18:44:15 +0300

    Add active items counter to footer

--- src/components/FilterFooter.jsx
+++ src/components/FilterFooter.jsx
@@ -12,7 +12,8 @@ export default class FilterFooter extends React.Component {
     }
 
     return (
-      <div className="col-8 mt-3 d-flex justify-content-around">
+      <div className="mt-3 d-flex justify-content-around">
+        <p>{`${list.filter(item => item.state === 'active').length} items left`}</p>
         {filters.map(f => {
           const className = f === filter ? "btn btn-secondary border-0" : "btn border-0";
           return <button className={className} key={f} onClick={onToggleFilter}>{f}</button>;
