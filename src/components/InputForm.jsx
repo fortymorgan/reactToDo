28aea3c9987830c04ce5fb1d77ff0a9a11b80294
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-21 15:49:36 +0300

    Add label to input form

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -7,8 +7,11 @@ export default class InputForm extends React.Component {
 
     return (
       <form className="form-inline" onSubmit={onAdd}>
-        <div className="form-group mx-sm-3">
-          <input type="text" value={value} onChange={onInput} />
+        <div className="input-group mx-sm-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="basic-addon1">New task</span>
+          </div>
+          <input type="text" className="form-control" placeholder="Enter new task" aria-label="Enter new task" aria-describedby="basic-addon1" value={value} onChange={onInput} />
         </div>
         <button type="submit" className="btn btn-primary btn-sm">Add</button>
       </form>
