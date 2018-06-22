03b45e114ab74444e21e01b7f4317ef5fbe1f11c
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-22 16:56:09 +0300

    Split footer to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import InputForm from './InputForm.jsx';
 import ListItems from './ListItems.jsx';
-import FilterFooter from './FilterFooter.jsx'
+import Footer from './FilterFooter.jsx'
 import { getItemsList, toLocalStorage } from '../scripts/storage';
 
 export default class App extends React.Component {
@@ -94,7 +94,7 @@ export default class App extends React.Component {
       <div className="jumbotron">
         <InputForm handlers={{ onInput: this.onInput, onAdd: this.onAdd, onToggleAll: this.onToggleAll }} value={input} list={items} />
         <ListItems handlers={{ onRemove: this.onRemove, onToggle: this.onToggle, onStartEdit: this.onStartEdit, onEdit: this.onEdit, onEndEdit: this.onEndEdit }} list={itemsToRender[filter]} />
-        <FilterFooter filter={filter} list={items} handlers={{ onToggleFilter: this.onToggleFilter, onClearFinished: this.onClearFinished }} />
+        <Footer filter={filter} list={items} handlers={{ onToggleFilter: this.onToggleFilter, onClearFinished: this.onClearFinished }} />
       </div>
     )
   }
--- src/components/FilterFooter.jsx
+++ src/components/FilterFooter.jsx
@@ -1,30 +1,35 @@
 import React from 'react';
 import cn from 'classnames';
 
-const filters = ['all', 'active', 'finished'];
+const Filters = (props) => {
+  const { filter, onToggleFilter } = props;
+  const filters = ['all', 'active', 'finished'];
 
-export default class FilterFooter extends React.Component {
-  render() {
-    const { filter, handlers, list } = this.props;
-    const { onToggleFilter, onClearFinished } = handlers;
-
-    if (list.length === 0) {
-      return null;
-    }
-
-    return (
-      <div className="mt-3 d-flex justify-content-around">
-        <span>{`${list.filter(item => item.state === 'active').length} items left`}</span>
-        {filters.map(f => {
-          const className = cn({
-            btn: true,
-            'btn-sm': true,
-            'btn-secondary': f === filter,
-          });
-          return <button className={className} key={f} onClick={onToggleFilter}>{f}</button>;
-          })}
-        <button className="btn btn-primary btn-sm" onClick={onClearFinished}>Clear finished</button>
-      </div>
-    )
-  }
+  return filters.map(f => {
+    const className = cn({
+      btn: true,
+      'btn-sm': true,
+      'btn-secondary': f === filter,
+    });
+    return <button className={className} key={f} onClick={onToggleFilter}>{f}</button>;
+  })
 }
+
+const Footer = (props) => {
+  const { filter, handlers, list } = props;
+  const { onToggleFilter, onClearFinished } = handlers;
+
+  if (list.length === 0) {
+    return null;
+  }
+
+  return (
+    <div className="mt-3 d-flex justify-content-around">
+      <span>{`${list.filter(item => item.state === 'active').length} items left`}</span>
+      <Filters filter={filter} onToggleFilter={onToggleFilter} />
+      <button className="btn btn-primary btn-sm" onClick={onClearFinished}>Clear finished</button>
+    </div>
+  )
+}
+
+export default Footer;
