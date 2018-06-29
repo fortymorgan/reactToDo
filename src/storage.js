cbd69db6f121ab0d065f1323b7cc6ab3b767e3c7
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-29 17:40:17 +0300

    Remove storage module

--- src/storage.js
+++
@@ -1,7 +0,0 @@
-export const getItemsList = () => (window.localStorage.getItem('todo-list') ?
-  JSON.parse(window.localStorage.getItem('todo-list')) : []);
-
-export const toLocalStorage = (key, data) => {
-  const jsonString = JSON.stringify(data);
-  window.localStorage.setItem(key, jsonString);
-};
