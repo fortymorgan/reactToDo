cdccbd1993c19aa8c455b52a45f3e32095e48922
Forty Morgan <dimazhmudikov@gmail.com>
2018-07-27 21:14:12 +0300

    Remove no auth options

--- src/components/NoAuth.jsx
+++ src/components/NoAuth.jsx
@@ -1,40 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import cn from 'classnames';
 
-const NoAuth = (props) => {
-  const { withoutAuth, toggleNoAuth } = props;
-
-  const toggleButtonClassName = cn({
-    btn: true,
-    'btn-check': true,
-    'btn-toggled': withoutAuth,
-  });
-
-  return (
-    <div className="auth">
-      <div className="auth-buttons">
-        <div className="auth-button">
-          <Link to="/signup">
-            <button type="button" className="btn">
-              Sign up
-            </button>
-          </Link>
-        </div>
-        <div className="auth-button">
-          <Link to="/signin">
-            <button type="button" className="btn">
-              Sign in
-            </button>
-          </Link>
-        </div>
+const NoAuth = () => (
+  <div className="auth">
+    <div className="auth-buttons">
+      <div className="auth-button">
+        <Link to="/signup">
+          <button type="button" className="btn">
+            Sign up
+          </button>
+        </Link>
       </div>
-      <div className="without-auth">
-        <button className={toggleButtonClassName} onClick={toggleNoAuth}>✓</button>
-        <div className="without-label">Without authorization</div>
+      <div className="auth-button">
+        <Link to="/signin">
+          <button type="button" className="btn">
+            Sign in
+          </button>
+        </Link>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
-export default NoAuth;
+export default NoAuth
--- src/components/NoAuthHeader.jsx
+++ src/components/NoAuthHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router';
 import { SignInFormContainer, SignUpFormContainer } from '../containers/Forms';
-import NoAuthContainer from '../containers/NoAuth';
+import NoAuth from './NoAuth.jsx';
 
 const NoAuthHeader = () => (
   <div className="app-header">
     <h1>To-do list</h1>
-    <Route exact path="/" component={NoAuthContainer} />
+    <Route exact path="/" component={NoAuth} />
     <Route path="/signin" component={SignInFormContainer} />
     <Route path="/signup" component={SignUpFormContainer} />
   </div>
